ef28eac821c1b0dddcb9a16440f7f4d2cf3e101b
Marco Strobel <mail@strobel.ch>
2014-08-01 22:58:08 +0200

    fix timezone test

--- pform/tests/test_timezone.py
+++ pform/tests/test_timezone.py
@@ -1,4 +1,5 @@
 import pytz
+from datetime import datetime
 from base import BaseTestCase
 
 
@@ -16,6 +17,7 @@ class TestTimezoneField(BaseTestCase):
         from pform import null, Invalid, TimezoneField
 
         typ = TimezoneField('test')
+        loc_dt = datetime(2002, 10, 27, 1, 30, 00)
 
         self.assertTrue(typ.to_field(null) is null)
         self.assertTrue(typ.to_field('') is null)
@@ -27,11 +29,13 @@ class TestTimezoneField(BaseTestCase):
                          "<StaticTzInfo 'Etc/GMT+6'>")
 
         # general timezones
-        self.assertEqual(repr(typ.to_field('US/Central')),
-                         "<DstTzInfo 'US/Central' CST-1 day, 18:00:00 STD>")
+        self.assertEqual(typ.to_field('US/Central') \
+                            .localize(loc_dt, is_dst=False).strftime('%Z%z'),
+                         "CST-0600")
 
-        self.assertEqual(repr(typ.to_field('us/central')),
-                         "<DstTzInfo 'US/Central' CST-1 day, 18:00:00 STD>")
+        self.assertEqual(typ.to_field('us/central') \
+                            .localize(loc_dt, is_dst=False).strftime('%Z%z'),
+                         "CST-0600")
 
         # unknown timezone
         self.assertRaises(Invalid, typ.to_field, 'unknown')
